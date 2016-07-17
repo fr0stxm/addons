0fe6263b7c635b3d35b92eed5c3777e6509c5340
fr0stxm <fr0stxm@users.noreply.github.com>
2016-07-17 13:38:17 +0100

    Added icon support, browser headers and colour to latest

--- plugin.video.jfh/default.py
+++ plugin.video.jfh/default.py
@@ -1,35 +1,39 @@
-import sys
-import os
-import xbmc
-import xbmcgui
-import xbmcplugin
-import xbmcaddon
-import urllib
-import urllib2
-import cookielib
-import re
+'''
+These addons are only possible because websites are open and allow us to view them for free.
+
+These addons are also only possible due to the numerous hours the kodi developers and addon developers put in to ensure that
+you, the user can have as much content as you need.
+
+However, it is incredibly clear that numerous cunts exist in the community and like nothing than to rip of the code of us,
+the hard working developers. You are known, we are watching.
+'''
+
+import sys, os, xbmc, xbmcgui, xbmcplugin, xbmcaddon, urllib, urllib2, cookielib, re
 
 settings = xbmcaddon.Addon(id='plugin.video.jfh')
 cookiejar = cookielib.LWPCookieJar()
 cookie_handler = urllib2.HTTPCookieProcessor(cookiejar)
 opener = urllib2.build_opener(cookie_handler)
-
+addon_id = 'plugin.video.jfh'
+selfAddon = xbmcaddon.Addon(id=addon_id)
+icon = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'icon.png'))
 
 def CATEGORIES():
     link = openURL('http://www.perfectgirls.net/')
     match = re.compile('<a href="/category/([0-9][0-9])/(.*)">(.*)</a>').findall(link)
-    addDir('Latest', 'http://www.perfectgirls.net/', 1, '', 1)
+    addDir('[COLOR red]Latest[/COLOR]', 'http://www.perfectgirls.net/', 1, icon, 1)
+    addDir('---', '', 1, '', 1)
     for page_id, channame, name in match:
         addDir(name,
                ('http://www.perfectgirls.net/category/' + page_id + '/' + channame),
-               1, '', 1)
+               1, icon, 1)
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 def VIDEOLIST(url):
     link = openURL(url)
     match = re.compile('-->\n<a href="/([0-9]+)/(.*)" title="(.*)">').findall(link)
     for v_id, videourl, name, in match:
-        addLink(name,'http://www.perfectgirls.net/' + v_id + '/' + videourl,2,'icon.png')
+        addLink(name,'http://www.perfectgirls.net/' + v_id + '/' + videourl,2,icon)
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
@@ -86,6 +90,7 @@ def addDir(name, url, mode, iconimage, page):
 
 def openURL(url):
     req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
     response = urllib2.urlopen(req)
     link = response.read()
     response.close()
