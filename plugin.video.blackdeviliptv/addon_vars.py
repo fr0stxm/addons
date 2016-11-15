cba21a26ba031badc513af324fbe894752c92381
fr0stxm <fr0stxm@users.noreply.github.com>
2016-11-15 20:51:24 +0000

    Added initial working copy of orbital streams (named blck devil)

---
+++ plugin.video.blackdeviliptv/addon_vars.py
@@ -0,0 +1,33 @@
+oid = 0
+panel = 33
+
+###IPTV###
+pvrID = 'pvr.iptvsimple'
+m3uUrlId = 'm3uUrl'
+epgUrlId = 'epgUrl'
+epgUrl = 'http://blackdevilslounge.co.uk/EPG/tvguide3.xml'
+
+#menus jsons
+
+main_menu = '''{
+  "menu": {
+    "id": "main",
+    "menuitems": [
+      {
+        "label": "My Account",
+        "mode": "my_account",
+        "folder": "True"
+      },
+      {
+        "label": "Live Television",
+        "mode": "live_tv",
+        "folder": "True"
+      },
+      {
+        "label": "Video On Demand",
+        "mode": "video_on_demand",
+        "folder": "True"
+      }
+    ]
+  }
+}'''
