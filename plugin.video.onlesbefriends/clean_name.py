45d9f1515dbd3ff832ed87ade349f5ff99b74e2f
oneil <oneilxm@protonmail.com>
2020-04-12 14:40:35 +0100

    Added thanks to clean_name.py

--- plugin.video.onlesbefriends/clean_name.py
+++ plugin.video.onlesbefriends/clean_name.py
@@ -1,3 +1,9 @@
+
+'''
+Owner: Kodification (No longer in the scene)
+
+Huge thanks to him for allowing me to use this slice of fried gold
+'''
 def clean_name(name):
 	name = name.replace('&amp;','&').replace('amp;','').replace('#039;','\'').replace('&acute;','\'').replace('&\'','\'').replace('&quot;','"').replace('</div>','')
 	name = name.replace('\n','').replace('<i class="icon_hd"></i>','')
